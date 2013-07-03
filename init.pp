8422cd94a701d25827950068169cfb16af593783
erik bergsma <erik@hyves.nl>
2013-07-03 17:50:42 +0200

    added ssh class

--- init.pp
+++ init.pp
@@ -1,9 +1,7 @@
-node erik-puppet-test {
-    include ntp
-}
+#include ntp
+include sshkeys
 
 class ntp {
-
     package { "ntp": 
         ensure => installed 
     }
@@ -11,4 +9,37 @@ class ntp {
     service { "ntp":
         ensure => running,
     }
+}
+
+class sshkeys {
+ 
+  ssh_keygen { 'root':
+    home => '/root/'
+  }
+
+  define ssh_keygen($home=undef) {
+
+    Exec { path => '/bin:/usr/bin' }
+
+    if $home == undef {
+      $home_real = "/home/${name}"
+    } else {
+      $home_real = $home
+    }
+
+    file { "$home_real/.ssh/":
+      ensure => "directory",
+      owner  => $name,
+      group  => $name,
+      mode   => 0755,
+      before => Exec["ssh_keygen-${name}"]
+    }
+
+    exec { "ssh_keygen-${name}":
+      command => "ssh-keygen -f \"${home_real}/.ssh/id_rsa\" -N '' -C '$hostname'",
+      user    => $name,
+      creates => "${home_real}/.ssh/id_rsa",
+    }
+
+  }
 }
