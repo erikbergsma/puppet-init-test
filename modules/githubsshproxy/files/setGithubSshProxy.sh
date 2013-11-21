d2d3a94598f00b2180144a2139d6dbd9acc50203
erik bergsma <erik@hyves.nl>
2013-11-21 17:34:48 +0100

    bugfix

--- modules/githubsshproxy/files/setGithubSshProxy.sh
+++ modules/githubsshproxy/files/setGithubSshProxy.sh
@@ -2,18 +2,19 @@
 
 #we need this in order to communicate with github
 
-homefolder=$(eval echo ~$(whoami))
-SSHConfigFile="${homefolder}/.ssh/config"
-VPN_IP=$(ip -4 addr list eth0 | awk '/inet/{split($2, a, "/"); print a[1]}' | sed -e 's/\.[0-9]*$/.2/g')
-
-while [[ "${VPN_IP}" == "" ]]; do
-	VPN_IP=$(ip -4 addr list eth0 | awk '/inet/{split($2, a, "/"); print a[1]}' | sed -e 's/\.[0-9]*$/.2/g')
-	sleep 1
-done
-
 grep github.com ${SSHConfigFile} 2>&1 >/dev/null
 
 if [[ ${?} -ne 0 ]]; then
+
+	homefolder=$(eval echo ~$(whoami))
+	SSHConfigFile="${homefolder}/.ssh/config"
+	VPN_IP=$(ip -4 addr list eth0 | awk '/inet/{split($2, a, "/"); print a[1]}' | sed -e 's/\.[0-9]*$/.2/g')
+
+	while [[ "${VPN_IP}" == "" ]]; do
+		VPN_IP=$(ip -4 addr list eth0 | awk '/inet/{split($2, a, "/"); print a[1]}' | sed -e 's/\.[0-9]*$/.2/g')
+		sleep 1
+	done
+
 	echo "host github.com" > ${SSHConfigFile}
 	echo "    user root" >> ${SSHConfigFile}
 	echo "    hostname github.com" >> ${SSHConfigFile}
