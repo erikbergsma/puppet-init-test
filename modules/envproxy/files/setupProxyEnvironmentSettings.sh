f9dfb72b3a3442466f3a3a2dfd0d696c4ef950c8
erik bergsma <erik@hyves.nl>
2013-11-26 13:47:25 +0100

    bugfix

--- modules/envproxy/files/setupProxyEnvironmentSettings.sh
+++ modules/envproxy/files/setupProxyEnvironmentSettings.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# Get Cloudp/bin/ipe VPN instance IP, should end on .2
-VPN_IP=$(/bin/ip -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
+IPBIN=$(which ip)
+
+# Get Cloudp${IPBIN}e VPN instance IP, should end on .2
+VPN_IP=$(${IPBIN} -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
 
 while [[ "${VPN_IP}" == "" ]]; do
-	VPN_IP=$(/bin/ip -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
+	VPN_IP=$(${IPBIN} -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
 	sleep 1
 done
 
-# Setup environment variables for cloudp/bin/ipe web proxy
+# Setup environment variables for cloudp${IPBIN}e web proxy
 /bin/echo -e "http_proxy=\"http://${VPN_IP}:3128\"
 HTTP_PROXY=\"http://${VPN_IP}:3128\"
 https_proxy=\"http://${VPN_IP}:3128\"
