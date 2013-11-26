f025e749693bdd75d038237089db79d5ccfcad77
erik bergsma <erik@hyves.nl>
2013-11-26 13:42:42 +0100

    bugfix

--- modules/githubsshproxy/files/setGithubSshProxy.sh
+++ modules/githubsshproxy/files/setGithubSshProxy.sh
@@ -6,14 +6,16 @@
 homefolder=$(eval /bin/echo ~$(whoami))
 SSHConfigFile="${homefolder}/.ssh/config"
 
+IPBIN=$(which ip)
+
 string=$(grep -o github.com ${SSHConfigFile} 2>/dev/null | head -n1)
 
 if [[ ${string} != "github.com" ]]; then
 
-	VPN_IP=$(/bin/ip -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
+	VPN_IP=$(${IPBIN} -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
 	
 	while [[ "${VPN_IP}" == "" ]]; do
-		VPN_IP=$(/bin/ip -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
+		VPN_IP=$(${IPBIN} -4 addr list eth0 | /usr/bin/awk '/inet/{split($2, a, "/"); print a[1]}' | /bin/sed -e 's/\.[0-9]*$/.2/g')
 	
 		/usr/bin/sleep 1
 	done
