f19336808ecf06307c05b1cd3d58a980b62a85d6
erik bergsma <erik@hyves.nl>
2013-11-26 15:50:10 +0100

    test for new setup

--- modules/githubsshproxy/manifests/init.pp
+++ modules/githubsshproxy/manifests/init.pp
@@ -1,25 +1,14 @@
 class githubsshproxy {
 
-  #
-  # the propper way to do this would be:
-  # in ruby / python:
-  # get factor local ip
-  # calculate the VPN ip based on that
-  # set the /root/.ssh/config file based on a template + VPN ip
-  #
+  $__ip = delete_at(split($::ipaddress, "[.]"), 3)
+  $__proxy_ip = join(concat($__ip,['2']), ".")
 
-  exec { "setGithubSshProxy":
-    command => "/bin/sh /tmp/setGithubSshProxy.sh",
-    path    => "/bin/bash:/bin/sh:/sbin:/usr/sbin"
-  }
-  
-  file { "/tmp/setGithubSshProxy.sh":
-    path    => "/tmp/setGithubSshProxy.sh",
+  file { "/root/.ssh/config":
+    path    => "/root/.ssh/config",
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source => "puppet:///modules/githubsshproxy/setGithubSshProxy.sh",
-    before => Exec["setGithubSshProxy"]
+    content => template('githubsshproxy/config.erb')
   }
 }
 
